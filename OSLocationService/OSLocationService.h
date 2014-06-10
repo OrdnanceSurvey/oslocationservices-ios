36f0108248e68201e580b948708d9700b6af1385
Layla Gordon <Layla.Gordon@ordnancesurvey.co.uk>
2014-06-10 11:57:38 +0100

    Initial Commit

---
+++ OSLocationService/OSLocationService.h
@@ -0,0 +1,13 @@
+//
+//  OSLocationService.h
+//  OSLocationService
+//
+//  Created by Layla Gordon on 10/06/2014.
+//  Copyright (c) 2014 Ordnance Survey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OSLocationService : NSObject
+
+@end
---
+++ OSLocationService/OSLocationService.m
@@ -0,0 +1,13 @@
+//
+//  OSLocationService.m
+//  OSLocationService
+//
+//  Created by Layla Gordon on 10/06/2014.
+//  Copyright (c) 2014 Ordnance Survey. All rights reserved.
+//
+
+#import "OSLocationService.h"
+
+@implementation OSLocationService
+
+@end
