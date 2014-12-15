30c0513a04ea256c14b30709833741cfc8595064
David Haynes (C) <David.Haynes@ordnancesurvey.co.uk>
2014-12-15 11:22:23 +0000

    Create new test host app for unit test running. Xcode 4 method of empty test host no longer works under Xcode 5.

---
+++ testest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testest
+//
+//  Created by David Haynes (C) on 15/12/2014.
+//  Copyright (c) 2014 Ordnance Survey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
