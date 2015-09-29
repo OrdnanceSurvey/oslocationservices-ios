491b8de82da179620b368ffaf6b67fccfc92dd54
Shri <Shikantreddy.Tekale@ordnancesurvey.co.uk>
2015-09-29 15:32:33 +0100

    MMI-453 : Minor refactor

--- OSLocationService/OSLocationProviderDelegate.h
+++ OSLocationService/OSLocationProviderDelegate.h
@@ -17,34 +17,34 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  *  Invoked when new locations are available.
  *
- *  @param manager   `OSLocationProvider` invoking the method
+ *  @param provider  `OSLocationProvider` invoking the method
  *  @param locations array of CLLocation objects in chronological order
  */
-- (void)locationProvider:(OSLocationProvider *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations;
+- (void)locationProvider:(OSLocationProvider *)provider didUpdateLocations:(NSArray<CLLocation *> *)locations;
 
 /**
  *  Invoked when a new heading is available
  *
- *  @param manager    `OSLocationProvider` invoking the method
+ *  @param provider   `OSLocationProvider` invoking the method
  *  @param newHeading the updated new heading
  */
-- (void)locationProvider:(OSLocationProvider *)manager didUpdateHeading:(CLHeading *)newHeading;
+- (void)locationProvider:(OSLocationProvider *)provider didUpdateHeading:(CLHeading *)newHeading;
 
 /**
  *  Invoked when an error has occurred.
  *
- *  @param manager `OSLocationProvider` invoking the method
- *  @param error   error describing the cause of failure. Error types are defined in "CLError.h".
+ *  @param provider `OSLocationProvider` invoking the method
+ *  @param error    error describing the cause of failure. Error types are defined in "CLError.h".
  */
-- (void)locationProvider:(OSLocationProvider *)manager didFailWithError:(NSError *)error;
+- (void)locationProvider:(OSLocationProvider *)provider didFailWithError:(NSError *)error;
 
 /**
  *  Invoked when the authorization status changes for the application
  *
- *  @param manager `OSLocationProvider` invoking the method
- *  @param status  the updated authorization status
+ *  @param provider `OSLocationProvider` invoking the method
+ *  @param status   the updated authorization status
  */
-- (void)locationProvider:(OSLocationProvider *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status;
+- (void)locationProvider:(OSLocationProvider *)provider didChangeAuthorizationStatus:(CLAuthorizationStatus)status;
 
 @end
 
